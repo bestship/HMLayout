3a282cb66ec2beec35a11f411bc1b001c82c4cbf
bestship <37078055+bestship@users.noreply.github.com>
2018-03-25 22:38:32 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LatihanLayout from './src/comp/LatihanLayout';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <LatihanLayout />
+    );
+  }
+}
